a78ca8a5e5e81c023d20459306d41eb95711d8d8
Seredenko23 <srvladyk555@gmail.com>
2020-11-19 12:02:26 +0200

    added gracefulShutdown

--- src/app.js
+++ src/app.js
@@ -4,6 +4,23 @@ const handler = require('./handler');
 
 const server = http.createServer(handler);
 
+function shutdownHandler() {
+  console.log('\nServer is closing...');
+  server.close(() => {
+    console.log('Server closed!');
+  });
+}
+
+function gracefulShutdown() {
+  process.on('SIGINT', shutdownHandler);
+  process.on('SIGTERM', shutdownHandler);
+
+  process.on('uncaughtException', shutdownHandler);
+  process.on('unhandledRejection', shutdownHandler);
+}
+
+gracefulShutdown();
+
 server.listen(+process.env.PORT, () => {
   console.log(`listening on port ${process.env.PORT}`);
 });
